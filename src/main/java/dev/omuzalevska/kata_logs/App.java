151e5d4fcff1c38e0597970b63cc437f48afac7b
OMuzalevska <oksana.muzalevskaja@gmail.com>
2025-01-06 22:07:25 +0100

    first commit

---
+++ src/main/java/dev/omuzalevska/kata_logs/App.java
@@ -0,0 +1,11 @@
+package dev.omuzalevska.kata_logs;
+
+
+public final class App {
+    private App() {
+    }
+
+public static void main(String[] args) {
+       // System.out.println("Hello World!");
+    }
+}
